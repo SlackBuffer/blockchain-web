9c2dbf88f589be86b5368aa3e8078006cc67e931
Slack Buffer <zoeyeooz@gmail.com>
2018-07-17 23:50:30 +0800

    fix two typos

--- projects/React/hello_redux/src/sagas/index.js
+++ projects/React/hello_redux/src/sagas/index.js
@@ -3,7 +3,7 @@ import { all, fork } from 'redux-saga/effects';
 import * as counter from './counter';
 import * as user from './user';
 
-export default function* rootSage() {
+export default function* rootSaga() {
   yield all([
     ...Object.values(counter),
     ...Object.values(user)
--- projects/React/reminder_pro/src/sagas/index.js
+++ projects/React/reminder_pro/src/sagas/index.js
@@ -2,7 +2,7 @@ import { all, fork } from 'redux-saga/effects';
 
 import * as subSaga from './subSaga';
 
-export default function* rootSage() {
+export default function* rootSaga() {
   yield all([
     ...Object.values(subSaga)
   ].map(fork));
